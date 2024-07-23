33378fd3521af1923dde6d08ec2a692aaa9a0146
Владислав Семыкин <vladislav_semykin01@mail.ru>
2024-07-23 19:42:55 +0300

    Added interface of the base timer

---
+++ TusurUI/TusurUI/Interfaces/IBaseTimer.cs
@@ -0,0 +1,25 @@
+﻿namespace TusurUI.Interfaces
+{
+    /**
+     * @interface IBaseTimerManager
+     * @brief Interface for managing a timer that executes a specific action at regular intervals.
+     *
+     * This interface provides methods to start and stop a timer, which executes a predefined action at specified intervals.
+     */
+    public interface IBaseTimerManager
+    {
+        /**
+         * @brief Starts the timer.
+         *
+         * This method starts the timer, which will execute a predefined action at regular intervals.
+         */
+        void Start();
+
+        /**
+         * @brief Stops the timer.
+         *
+         * This method stops the timer, preventing it from executing any further actions.
+         */
+        void Stop();
+    }
+}
