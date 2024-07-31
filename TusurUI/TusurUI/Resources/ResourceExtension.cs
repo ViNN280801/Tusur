899eb1a2350cefeab03ca7be799ea7f392fad1da
Владислав Семыкин <vladislav_semykin01@mail.ru>
2024-07-31 15:51:17 +0300

    Added resources for the dual languages (ru/en)

---
+++ TusurUI/TusurUI/Resources/ResourceExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Markup;
+using TusurUI.Errors;
+
+namespace TusurUI.Resources
+{
+    public class ResourceExtension : MarkupExtension
+    {
+        public string? Key { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Key == null)
+                return string.Empty;
+
+            var value = ErrorMessages.GetErrorMessage(Key);
+            return value ?? string.Empty;
+        }
+    }
+}
---
+++ TusurUI/TusurUI/Resources/ResourceHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace TusurUI.Resources
+{
+    public static class ResourceHelper
+    {
+        public static void ChangeCulture(string cultureCode)
+        {
+            CultureInfo newCulture = new CultureInfo(cultureCode);
+            CultureInfo.CurrentCulture = newCulture;
+            CultureInfo.CurrentUICulture = newCulture;
+        }
+    }
+}
